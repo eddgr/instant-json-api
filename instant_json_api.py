80d0d01a6c25734e1b804f59ed364363e49a6438
Edgar Ong <emailedgar@gmail.com>
2020-11-21 20:59:33 -0500

    (feat): build wheel file to be used with current Flask projects

---
+++ instant_json_api.py
@@ -0,0 +1,54 @@
+from functools import partial
+import json
+import os
+
+from flask import Flask
+from flask_cors import CORS
+
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def _api_name(filename):
+    return '/api/{}'.format(filename), filename
+
+
+def _create_route(payload=None):
+    return payload
+
+
+class InstantJsonApi(object):
+    def __init__(self, app=None, port=None):
+        if app is None:
+            app = Flask(__name__)
+            CORS(app)
+        if port is None:
+            port = 9001
+        self.app = app
+        self.port = port
+        self.init_app(app)
+    
+    def init_app(self, app):
+        """
+        Initialize the app to create routes for the JSON files.
+        """
+        for file in os.listdir(current_path):
+            if file.endswith('.json'):
+                try:
+                    with open(file) as json_file:
+                        payload = json.load(json_file)
+                        parsed_filename = file.split('.')[0]
+                        route, name = _api_name(parsed_filename)
+                        app.add_url_rule(
+                            route, name, partial(
+                                _create_route, payload=payload))
+                except:
+                    raise Exception(
+                        '{} has no values or is not using valid JSON format'.format(
+                            file))
+
+    def run(self):
+        """
+        Use for standalone run.
+        """
+        return self.app.run(port=self.port, debug=True)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='Instant-JSON-API',
+    version='1.0',
+    url='https://github.com/eddgr/instant-json-api',
+    author='Edgar Ong',
+    author_email='emailedgar@gmail.com',
+    description='Create instant API endpoints for your JSON files.',
+    py_modules=['instant_json_api'],
+    install_requires=['Flask'],
+    license='MIT'
+)
